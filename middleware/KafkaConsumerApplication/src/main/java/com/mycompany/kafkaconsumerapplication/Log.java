03384de6803564d1084f6188df82b98f165e63c4
ajbill <31999393+ajbill@users.noreply.github.com>
2019-03-17 22:55:08 +0000

    Update Log.java

--- middleware/KafkaConsumerApplication/src/main/java/com/mycompany/kafkaconsumerapplication/Log.java
+++ middleware/KafkaConsumerApplication/src/main/java/com/mycompany/kafkaconsumerapplication/Log.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.kafkaconsumerapplication;
 
 import java.io.BufferedReader;
@@ -12,58 +7,56 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.simple.JSONObject;
 
-/**
- *
- * @author andrew
- */
-public class Log {
-    
-    
-public String sendNewLog(String urlpath, JSONObject json) {
-        HttpURLConnection connection = null;
-        try {
-            URL url=new URL(urlpath);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Accept", "application/json");
-            OutputStreamWriter streamWriter = new OutputStreamWriter(connection.getOutputStream());
-            streamWriter.write(json.toString());
-            streamWriter.flush();
-            StringBuilder stringBuilder = new StringBuilder();
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-                InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(streamReader);
-                String response = null;
-                while ((response = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(response + "\n");
-                }
-                bufferedReader.close();
+public class Log { 
 
-                System.out.println(stringBuilder.toString());
-                return stringBuilder.toString();
-            } else {
-                //Log.e("test", connection.getResponseMessage());
-                System.out.println(connection.getResponseMessage());
-                return null;
-            }
+    public void newLog(String deviceId, String log, String logCount) {
+    
+        HttpURLConnection connection = null;
+    
+        JSONObject jsonLog = new JSONObject();
+    
+        jsonLog.put("$class", "org1.andrew.lognetwork.AddNewLog");
+        jsonLog.put("deviceId", deviceId);
+        jsonLog.put("newLog", log);
+        jsonLog.put("logCount", logCount);
+    
+        String response = new String();
+
+        try {
+                URL url=new URL("http://localhost:3000/api/AddNewLog");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json"); 
+                connection.setRequestProperty("Connection", "Keep-Alive");
+
+                OutputStreamWriter streamWriter = new OutputStreamWriter(connection.getOutputStream());
+                streamWriter.write(jsonLog.toString());
+                streamWriter.flush();
+                streamWriter.close();
+                StringBuilder stringBuilder = new StringBuilder();
+                    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                        InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+                        BufferedReader bufferedReader = new BufferedReader(streamReader);
+
+                        while ((response = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(response + "\n");
+                        }
+                            bufferedReader.close();
+                            System.out.println(stringBuilder.toString());
+                            streamReader.close();
+                        } else {
+                            System.out.println(connection.getResponseMessage());
+                        }
+
         } catch (Exception exception){
-            System.out.println(exception.toString());
-            //Log.e("test", exception.toString());
-            return null;
+                System.out.println(exception.toString());
         } finally {
-            if (connection != null){
-                connection.disconnect();
-            }
+                if (connection != null){
+                    connection.disconnect();
+                }
         }
-    }   
-    
-    
-    
-    
-    
-    
-    
+    }  
 }
